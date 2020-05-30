271ed2754700d91d3df09e75102bef446f9ba624
yhl234 <200392342@student.georgianc.on.ca>
2020-05-30 15:58:58 -0400

    cleaned up projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import ProjectsList from '../components/ProjectsList'
 
-const Projects = ({ className }) => {
+const Projects = () => {
   return (
     <Layout>
       <SEO title="Projects" />
       <ProjectsList />
     </Layout>
   )
-}
-
-Projects.propTypes = {
-  className: PropTypes.string,
 }
 
 export default Projects
